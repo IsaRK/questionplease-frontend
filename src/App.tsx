91b2a3d291039d54b2b5c0edca7838aff6dbeb71
IsaRK <isabelle.riverain@gmail.com>
2021-02-06 23:11:09 +0100

    clean authorization code flow

--- src/App.tsx
+++ src/App.tsx
@@ -18,61 +18,8 @@ export const useStyles = makeStyles((theme) => ({
 }));
 
 export const App: React.FC = () => {
-  /*
-  // current authenticated user
-  const [currentUser, setCurrentUser] = useState<AccountInfo>();
-
-  // authentication callback
-  const onAuthenticated = async (userAccountInfo: AccountInfo) => {
-    setCurrentUser(userAccountInfo);
-  };
-
-  // Render JSON data in readable format
-  const PrettyPrintJson = ({ data }: any) => {
-    return (
-      <div>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      </div>
-    );
-  };
-
-  // Quick link - user revokes app's permission
-  const ShowPermissionRevokeLinks = () => {
-    return (
-      <div>
-        <div>
-          <a href="https://myapps.microsoft.com" target="_blank" rel="noopener">
-            Revoke AAD permission
-          </a>
-        </div>
-        <div>
-          <a
-            href="https://account.live.com/consent/manage"
-            target="_blank"
-            rel="noopener"
-          >
-            Revoke Consumer permission
-          </a>
-        </div>
-      </div>
-    );
-  };
-  */
-
   return (
     <StyledBox>
-      {/*
-      <div id="App">
-        <h2>Microsoft Login Button application</h2>
-        <AzureAuthenticationButton onAuthenticated={onAuthenticated} />
-        {currentUser && (
-          <div>
-            <PrettyPrintJson data={currentUser} />
-            <ShowPermissionRevokeLinks />
-          </div>
-        )}
-      </div>
-        */}
       <SignInButton />
       <QuestionComputer />
       <AnswerForm />
